2b51ffc4ab76761a7ca20cf4a63ec3a5892b7e63
Gabriel Piltao <40001302+GabrielPilato@users.noreply.github.com>
2023-03-19 20:00:07 -0300

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int INDICE = 13;
+    int SOMA = 0;
+    int K = 0;
+
+    SOMA = SOMA + 3;
+
+    cout << "O resultado de Soma eh: " << SOMA;
+
+
+
+}
