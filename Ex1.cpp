a0f192e0a6f6ce2eb5afe0276bef1c5feee50c1a
Gabriel Piltao <40001302+GabrielPilato@users.noreply.github.com>
2023-03-19 23:16:39 -0300

    Update Ex1.cpp

--- Ex1.cpp
+++ Ex1.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+//Ola, obrigado por olhar o meu código, Por gentileza, caso eu tenha passado no processo, 
+//me contactar via email, pois aonde eu trabalho não tenho área de celular, Muito obrigado!
+
 int main() {
 
     int INDICE = 13;
